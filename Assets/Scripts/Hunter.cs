a54d071bbc7117fb8e3c174f92900acb8e869f5d
Josh Brackett <joshua.brackett@hotmail.ca>
2018-01-31 14:28:04 -0500

    fixed where the if disablemovement was in the playercontroller also set up the warrior script so he can charge and whirlwind now

---
+++ Assets/Scripts/Hunter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hunter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// if(m_Disengage) {
+			// 	m_moveDirection.y = m_jumpSpeed;
+			// 	m_moveDirection.z = -m_jumpSpeed;
+			// 	m_Disengage = false;
+			// } else {	
+			// 	m_animationController.SetBool("Movement",false);
+			// }
+
+			// if(m_grounded) {
+			// 	m_Disengage = false;
+			// }
+		
+	}
+}
